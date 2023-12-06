48c80233cc28c7592d2dbb52a3c8db5d4f13f1bf
svamuefs <145811832+svamuefs@users.noreply.github.com>
2023-12-06 12:59:08 -0300

    criar mux2x

---
+++ mux2x1.v
@@ -0,0 +1,14 @@
+module mux2x1 (
+    input in_a , in_b , select , enable
+
+    output out
+);
+
+    not not0 (notSelect , select);
+
+    and and0 (and0_w , in_a , notSelect , enable);
+    and and1 (and1_w , in_b , select , enable);
+
+    or or0 (out_w , and0_w , and1_w);
+    
+endmodule
