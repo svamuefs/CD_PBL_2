933415185e5715a4678ad8590608ac4ad1ed7a8f
svamuefs <145811832+svamuefs@users.noreply.github.com>
2023-12-06 13:14:16 -0300

    Atualizar o d_flipflop.v
    D flipflops agora possuem configuração para valor inicial

--- d_flipflop.v
+++ d_flipflop.v
@@ -1,7 +1,12 @@
 module d_flipflop (
-    input d , clk , reset ,
+    input d , clk , reset , initial_value ,
     output reg out
 );
+
+		initial begin
+				if (initial_value)
+						out <= 1;
+end
 
     always @(posedge clk or negedge reset) begin
 
