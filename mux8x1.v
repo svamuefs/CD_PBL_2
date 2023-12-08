81a0a78e062da4f7407e554d0fed578fc59a2d70
svamuefs <145811832+svamuefs@users.noreply.github.com>
2023-12-08 13:05:25 -0300

    Atualizar o mux8x1.v

--- mux8x1.v
+++ mux8x1.v
@@ -1,43 +1,3 @@
-// module mux8x1 #(parameter DATA_WIDTH = 35) 
-// (
-//     input [DATA_WIDTH-1:0] in_a , in_b , in_c , in_d , in_e , in_f , in_g , in_h ,
-//     input [2:0] select , 
-//     input enable ,
-
-//     output [DATA_WIDTH-1:0] out
-// );
-
-//     wire [DATA_WIDTH-1:0] mux1_w , mux2_w , and1_w , and2_w;
-
-//     not not1 (notSelect2 , select[2]);
-
-//     mux4x1 mux4x1_1 (
-//         .in_a   (in_a) ,
-//         .in_b   (in_b) ,
-//         .in_c   (in_c) ,
-//         .in_d   (in_d) ,
-//         .select (select[1:0]) ,
-//         .enable (notSelect2) ,
-
-//         .out    (mux1_w)
-//     );
-
-//     mux4x1 mux4x1_2 (
-//         .in_a   (in_e) ,
-//         .in_b   (in_f) ,
-//         .in_c   (in_g) ,
-//         .in_d   (in_h) ,
-//         .select (select[1:0]) ,
-//         .enable (select[2]) ,
-
-//         .out    (mux2_w)
-//     );
-
-//     or or1 [34:0] (out , mux1_w , mux2_w);
-
-    
-// endmodule
-
 module mux8x1
 (
     input in_a , in_b , in_c , in_d , in_e , in_f , in_g , in_h ,
@@ -47,37 +7,34 @@ module mux8x1
     output out
 );
 
-    wire mux1_w , mux2_w , and1_w , and2_w;
-
-    not not1 (notSelect2 , select[2]);
-
-    //enable
-
-    and andEnable1 (enable1 , notSelect2 , enable);
-    and andEnable2 (enable2 , select[2] , enable);
-
-    mux4x1 mux4x1_1 (
+    mux4x1 mux_0 (
         .in_a   (in_a) ,
         .in_b   (in_b) ,
         .in_c   (in_c) ,
         .in_d   (in_d) ,
         .select (select[1:0]) ,
-        .enable (enable1) ,
+        .enable (enable) ,
 
-        .out    (mux1_w)
+        .out    (mux0_w)
     );
 
-    mux4x1 mux4x1_2 (
+    mux4x1 mux_1 (
         .in_a   (in_e) ,
         .in_b   (in_f) ,
         .in_c   (in_g) ,
         .in_d   (in_h) ,
         .select (select[1:0]) ,
-        .enable (enable2) ,
+        .enable (enable) ,
 
-        .out    (mux2_w)
+        .out    (mux1_w)
     );
 
-    or or1 (out , mux1_w , mux2_w);
+mux2x1 mux_2 (
+        .in_a   (mux0_w) ,
+        .in_b   (mux1_w) ,
+        .select (select[1:0]) ,
+        .enable (enable) ,
 
+        .out    (out)
+    );
 endmodule
